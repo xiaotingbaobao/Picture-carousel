46ebde91f52b1e7e2cd1689bf08388be7ff03efa
ting <924547237@qq.com>
2019-06-26 18:25:33 +0800

    图片轮换~

---
+++ move3.js
@@ -0,0 +1,47 @@
+function getStyle(obj,name)
+{
+	if(obj.currentStyle)
+	{
+		return obj.currentStyle[name];
+	}
+	else
+	{
+		return getComputedStyle(obj,false)[name];
+	}
+}
+
+function startMove(obj, attr, iTarget,fnEnd)
+{
+	clearInterval(obj.timer);
+	obj.timer=setInterval(function()
+	{
+		var cur=0;
+		if(attr=='opacity')
+		{
+			cur=Math.round(parseFloat(getStyle(obj,attr))*100)
+		}else{
+			cur=parseInt(getStyle(obj,attr));
+		}
+		
+	
+		
+		var speed=(iTarget-cur)/6;
+		speed=speed>0?Math.ceil(speed):Math.floor(speed);
+		
+		if(cur==iTarget)
+		{
+			clearInterval(obj.timer);
+			if(fnEnd)fnEnd();
+		}else{
+			if(attr=='opacity')
+			{
+				obj.style.opacity=(cur+speed)/100;
+				obj.style.alpha='alpha(opacity:'+(cur+speed)+')';
+			}
+			else
+			{
+				obj.style[attr]=cur+speed+'px';
+			}
+		}
+	},30);
+}
